18c5e33ff22a10b521be675525481b2844ae84e4
Matt Van Vleet <mattvanvleet@Matts-Air.attlocal.net>
2016-08-07 23:45:24 -0400

    Simpllest thing toget the sort working
    ls
    git diff

--- src/Calculator.c
+++ src/Calculator.c
@@ -7,7 +7,13 @@
 char* add(const char* val1, const char* val2){
   int len =strlen(val1)+strlen(val1)+1;
   char* concatinatedValue=malloc(len);
-  strcpy(concatinatedValue,val1);
-  strcat(concatinatedValue,val2);
+  if ('V' == *val1) {
+		strcpy(concatinatedValue,val1);
+	  	strcat(concatinatedValue,val2);
+	}
+  else{
+  		strcpy(concatinatedValue,val2);
+  		strcat(concatinatedValue,val1);
+ 	} 
   return concatinatedValue;
 }
