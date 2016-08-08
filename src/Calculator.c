aa6d8e96315181e7f545bb0abc481121d274198a
Matt Van Vleet <mattvanvleet@Matts-Air.attlocal.net>
2016-08-08 18:19:50 -0400

    Extracting More Methods

--- src/Calculator.c
+++ src/Calculator.c
@@ -10,15 +10,18 @@ static void concatRomans(char* concatinatedValue, const char* val1, const char* 
   strcat(concatinatedValue,val2);
 }
 
-static void sortRomans(char* sortedReturnValue, const char* concatinatedValue){
-   int c = 0, count[26] = {0};
+static void determineRomanFrequency(int frequencyArray[26], const char* concatinatedValue){
+   int c = 0;
    while (concatinatedValue[c] != '\0')
    {
-      count[concatinatedValue[c]-'A']++;
+      frequencyArray[concatinatedValue[c]-'A']++;
       c++;
    }
- 
+ }
+
+static void writeProperlyFormattedRomanNumeral(char* sortedReturnValue, const int count[26]){
    static const char ALL_ROMANS[] = "MDCLXVI";
+
    int d = 0;
    char* p=sortedReturnValue;
    while (ALL_ROMANS[d] != '\0')
@@ -32,6 +35,12 @@ static void sortRomans(char* sortedReturnValue, const char* concatinatedValue){
 
 }
 
+static void sortRomans(char* sortedReturnValue, const char* concatinatedValue){
+   int count[26] = {0};
+   determineRomanFrequency(count,concatinatedValue);
+   writeProperlyFormattedRomanNumeral(sortedReturnValue,count);
+}
+
 char* add(const char* val1, const char* val2){
 
   int len =strlen(val1)+strlen(val1)+1;
@@ -40,7 +49,6 @@ char* add(const char* val1, const char* val2){
 
   char* sortedReturnValue=malloc(strlen(concatinatedValue)+1);
   sortRomans(sortedReturnValue,concatinatedValue);
-
 
   return sortedReturnValue;
 }
