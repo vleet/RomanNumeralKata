f449323f460cc0db9a61f89fa58e39384b056181
Matt Van Vleet <mattvanvleet@Matts-Air.attlocal.net>
2016-08-07 22:59:09 -0400

    First Passing Test

--- src/Calculator.c
+++ src/Calculator.c
@@ -5,5 +5,9 @@
 #include <stdbool.h>
 
 char* add(const char* val1, const char* val2){
-  return (val1);
+  int len =strlen(val1)+strlen(val1)+1;
+  char* sum=malloc(len);
+  strcpy(sum,val1);
+  strcat(sum,val2);
+  return sum;
 }
